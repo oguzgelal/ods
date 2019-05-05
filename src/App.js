2598ac763492da433e1a9a17e661774c92f55b76
Oguz Gelal <o.gelal77@gmail.com>
2019-05-05 14:31:49 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
+import styled from 'styled-components/macro';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Wrapper = styled.div``;
+
+const App = props => (
+  <Wrapper>
+    Hello world!
+  </Wrapper>
+);
+
+App.propTypes = {
+};
 
 export default App;
